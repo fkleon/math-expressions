43edfdce362ab0a544aa83fd1847c8fb5a2e5680
Frederik Leonhardt <frederik.leonhardt@gmail.com>
2013-10-10 15:16:26 -0400

    Improve tests.

--- test/parser_test_set.dart
+++ test/parser_test_set.dart
@@ -8,8 +8,10 @@ class ParserTests extends TestSet {
   get name => 'Parser Tests';
 
   get testFunctions => {
-    'Lexer Token Creation (Infix + Postfix)': lexerTokenTest,
-    'Parser Expression Creation': parserExpressionTest
+    'Lexer Tokenize (Infix + Postfix)': lexerTokenTest,
+    'Lexer Tokenize Invalid': lexerTokenTestInvalid,
+    'Parser Expression Creation': parserExpressionTest,
+    'Parser Expression Creation Invalid': parserExpressionTestInvalid
   };
 
   void initTests() {
@@ -17,11 +19,12 @@ class ParserTests extends TestSet {
     lex = new Lexer();
 
     inputStrings = ["x + 2",
-                    "x * 2^2.5 * log(10)(100)",
-                    "log(10)(100)",
+                    "x * 2^2.5 * log(10,100)",
+                    "log(10,100)",
                     "0 - 1",
                     "(0 - 1)",
-                    //"-1"
+                    "nrt(5,10-1)",
+                    //"(-1)"
                     ];
     
     tokenStreams = new List(inputStrings.length);
@@ -44,8 +47,7 @@ class ParserTests extends TestSet {
                       new Token("log", TokenType.LOG),
                       new Token("(", TokenType.LBRACE),
                       new Token("10", TokenType.VAL),
-                      new Token(")", TokenType.RBRACE),
-                      new Token("(", TokenType.LBRACE),
+                      new Token(",", TokenType.SEPAR),
                       new Token("100", TokenType.VAL),
                       new Token(")", TokenType.RBRACE)];
 
@@ -53,17 +55,16 @@ class ParserTests extends TestSet {
                           new Token("2", TokenType.VAL),
                           new Token("2.5", TokenType.VAL),
                           new Token("^", TokenType.POW),
+                          new Token("*", TokenType.TIMES),
                           new Token("10", TokenType.VAL),
                           new Token("100", TokenType.VAL),
                           new Token("log", TokenType.LOG),
-                          new Token("*", TokenType.TIMES),
                           new Token("*", TokenType.TIMES)];
 
     tokenStreams[2] = [new Token("log", TokenType.LOG),
                     new Token("(", TokenType.LBRACE),
                     new Token("10", TokenType.VAL),
-                    new Token(")", TokenType.RBRACE),
-                    new Token("(", TokenType.LBRACE),
+                    new Token(",", TokenType.SEPAR),
                     new Token("100", TokenType.VAL),
                     new Token(")", TokenType.RBRACE)];
     
@@ -88,13 +89,27 @@ class ParserTests extends TestSet {
     rpnTokenStreams[4] = [new Token("0", TokenType.VAL),
                           new Token("1", TokenType.VAL),
                           new Token("-", TokenType.MINUS)];
-    /*
-    tokenStreams[5] = [new Token("-", TokenType.UNMINUS),
-                       new Token("1", TokenType.VAL)];
     
-    rpnTokenStreams[5] = [new Token("1", TokenType.VAL),
-                          new Token("-", TokenType.UNMINUS)];
-    */
+    tokenStreams[5] = [new Token("nrt", TokenType.ROOT),
+                       new Token("(", TokenType.LBRACE),
+                       new Token("5", TokenType.VAL),
+                       new Token(",", TokenType.SEPAR),
+                       new Token("10", TokenType.VAL),
+                       new Token("-", TokenType.MINUS),
+                       new Token("1", TokenType.VAL),
+                       new Token(")", TokenType.RBRACE)];
+    
+    rpnTokenStreams[5] = [new Token("5", TokenType.VAL),
+                          new Token("10", TokenType.VAL),
+                          new Token("1", TokenType.VAL),
+                          new Token("-", TokenType.MINUS),
+                          new Token("nrt", TokenType.ROOT)];
+    
+//    tokenStreams[6] = [new Token("-", TokenType.MINUS),
+//                       new Token("1", TokenType.VAL)];
+//    
+//    rpnTokenStreams[6] = [new Token("1", TokenType.VAL),
+//                          new Token("-", TokenType.UNMINUS)];
   }
 
   /*
@@ -103,7 +118,7 @@ class ParserTests extends TestSet {
   Parser pars;
   Lexer lex;
   List<String> inputStrings;
-
+  
   List<List<Token>> tokenStreams;
   List<List<Token>> rpnTokenStreams;
 
@@ -120,12 +135,44 @@ class ParserTests extends TestSet {
       expect(rpnStream, orderedEquals(rpnTokenStreams[i]));
     }
   }
+  
+  void lexerTokenTestInvalid() {
+    Map invalidCases = {
+                        '(': throwsStateError,
+                        ')': throwsStateError,
+                        '1+1)': throwsStateError,
+                        '(1+1': throwsStateError,
+//                        'log(,1)': throws,
+//                        'log(1,)': throws,
+                        'log(1,': throwsStateError,
+    };
+    
+    for (String expr in invalidCases.keys) {
+      expect(() => lex.tokenizeToRPN(expr), invalidCases[expr]);
+    }
+  }
 
   void parserExpressionTest() {
     for (String inputString in inputStrings) {
       Expression exp = pars.parse(inputString);
       // TODO Don't just test for no exceptions,
       // also test for expression content.
+    }
+  }
+  
+  void parserExpressionTestInvalid() {
+    Map invalidCases = {
+                        '': throwsArgumentError,
+                        '(': throwsStateError,
+                        ')': throwsStateError,
+                        '1+1)': throwsStateError,
+                        '(1+1': throwsStateError,
+                        'log(,1)': throwsRangeError,
+                        'log(1,)': throwsRangeError,
+    };
+    
+    for (String expr in invalidCases.keys) {
+      expect(() => pars.parse(expr), invalidCases[expr]);
     }
   }
 }
