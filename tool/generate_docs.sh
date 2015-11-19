4e9b5c51fc36424cb5328ff237ef3b1648de4407
Frederik Leonhardt <frederik.leonhardt@gmail.com>
2015-11-19 22:34:46 +1300

    Remove document generation script in favour of dartdoc

--- tool/generate_docs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Creates dart doc, excludes third party libraries from being documented
-$DART_SDK/bin/dartdoc --no-show-private --no-code --package-root=../packages/ --out=docs --include-lib math_expressions,math_expressions_test,test_framework -v "../test/math_expressions_test.dart"
